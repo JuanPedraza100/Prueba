ad609221020448bfc444465560d926c84fcbac11
JuanPedraza100 <55706651+JuanPedraza100@users.noreply.github.com>
2019-10-01 19:51:50 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//USING SIRVE PARA INVOCAR LIBRERIAS REFERENCIADAS PREVIAMENTE.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//NAMESPACE CONTIENE UNA CLASE Y UN METODO QUE ES LO QUE EJECUTARA EL PROGRAMA.
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo, ahora en C#!");
+            //Pausa
+            Console.ReadLine();
+        }
+    }
+}
